bcf4c3ea1c00c44040b88e7fd6bbb1f30aee85a2
Emile Donzel <emiledonzel@bluewin.ch>
2024-01-25 11:26:16 +0100

    modif exo 01

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-const test = 4;
+import { generatePalette } from "./modules/utils";
+
+const chooseColor = document.querySelector('input');
+
+chooseColor.addEventListener("input", function (e){
+    e.preventDefault;
+    console.log("test");
+})
+a
